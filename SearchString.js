839655c266350e2f11896549976c976e1752e64f
hvphan1993 <hvphan1993@gmail.com>
2022-10-25 21:26:46 -0700

    track document stats and search for terms

--- SearchString.js
+++ SearchString.js
@@ -46,9 +46,11 @@ function getDocStats(fileContent) {
     // filter out uncommon words
     uncommonWords = filterStopWords(wordArray);
 
+
+
     // Count every word in the wordArray
-    for( let word in wordArray ){
-        let wordValue = wordArray[word]
+    for( let word in uncommonWords ){
+        let wordValue = uncommonWords[word]
         if (wordDictionary[wordValue] > 0) {
             wordDictionary[wordValue] += 1;
         }
@@ -68,6 +70,9 @@ function getDocStats(fileContent) {
     // write the value to the page
     ULTemplate(top5Words,document.getElementById("mostUsed"));
     ULTemplate(least5Words, document.getElementById("leastUsed"));
+
+    docLength.innerText = "Document Length: " + text.length;
+    wordCount.innerText = "Word Count: " + wordArray.length;
 }
 
 function ULTemplate(items,element){
@@ -100,8 +105,8 @@ function filterStopWords(wordArray) {
     var commonObj = {};
     var uncommonArr = [];
 
-    for (i = 0; i < commonObj; i++) {
-        commonObj[common[i].trim()] = true;
+    for (i = 0; i < commonWords.length; i++) {
+        commonObj[commonWords[i].trim()] = true;
     }
 
     for (i = 0; i < wordArray.length; i++) {
@@ -118,3 +123,41 @@ function filterStopWords(wordArray) {
 function getStopWords() {
     return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
 }
+
+// highlight the words in the search
+function performMark() {
+
+
+    // read the keyword 
+    var keyword = document.getElementById("keyword").value;
+    var display = document.getElementById("fileContent");
+
+    var newContent = "";
+
+    // find all the currently marked items
+    let spans = document.querySelectorAll('mark');
+    
+    // <mark>Harry</mark>
+    // Harry
+
+    for ( var i = 0; i < spans.length; i++) {
+        spans[i].outerHTML = spans[i].innerHTML;
+    }
+
+    var re = new RegExp(keyword, "gi" );
+    var replaceText = "<mark id='markme'>$&</markme>";
+    var bookContent = display.innerHTML;
+
+    // add the mark to the book content
+    newContent = bookContent.replace(re,replaceText);
+
+    display.innerHTML = newContent;
+    var count = document.querySelectorAll('mark').length;
+    document.getElementById("searchstat").innerHTML = "found " + count + " matches";
+
+    if (count > 0) {
+        var element = document.getElementById("markme");
+        element.scrollIntoView();
+    }
+
+};
